f7b0e4cb7cd336d053e7a94a3038e7f3fdd5746e
Losiowaty <elch1989@gmail.com>
2013-04-20 13:33:32 +0200

    nadal walczymy

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    for (int i = 0; i < 2; ++i);
+    for (int i = 0; i < 2; ++i) {
+        //do nothing
+    }
     
     return a.exec();
 }
